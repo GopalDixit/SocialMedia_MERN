fb4d237d53b24a7444e8db8dd95682e81233395c
Gopal Dixit <98686075+GopalDixit@users.noreply.github.com>
2024-10-11 02:29:49 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,32 @@
 import React, { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Feed from './components/Feed';
 import Login from './components/Login';
 import Register from './components/Register';
-import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedRoute from './components/ProtectedRoute'; 
 
 const App = () => {
-  const [userId, setUserId] = useState(null);
+  const [userId, setUserId] = useState(null); 
   const [username, setUsername] = useState('');
 
   return (
-    <Routes>
-      <Route 
-        path="/" 
-        element={<ProtectedRoute element={<Login setUserId={setUserId} setUsername={setUsername} />} authRoute={true} />} 
-      />
-      <Route 
-        path="/register" 
-        element={<Register />}
-      />
-      <Route 
-        path="/feed" 
-        element={<ProtectedRoute element={<Feed userId={userId} username={username} setUserId={setUserId} setUsername={setUsername} />} />} 
-      />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route 
+          path="/" 
+          element={<ProtectedRoute element={<Login setUserId={setUserId} setUsername={setUsername} />} authRoute={true} />} 
+        />
+        <Route 
+          path="/register" 
+          element={<Register />}
+        />
+
+        <Route 
+          path="/feed" 
+          element={<ProtectedRoute element={<Feed userId={userId} username={username} setUserId={setUserId} setUsername={setUsername} />} />} 
+        />
+      </Routes>
+    </Router>
   );
 };
 
