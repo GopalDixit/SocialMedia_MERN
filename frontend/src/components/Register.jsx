dfa84ac54c7bda84df5c6a10528e0961632dabec
Gopal Dixit <98686075+GopalDixit@users.noreply.github.com>
2024-10-11 00:15:52 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:4800/register', {
+      await axios.post('https://socialmedia-mern-509c.onrender.com/register', {
         username,
         email,
         password,
